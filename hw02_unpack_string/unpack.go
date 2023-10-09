7e04f7afaf7acf5d921e0ce5daf3853d66ad0023
niknamess <nikitanuc@yandex.ru>
2023-10-09 18:53:25 +0300

    *fix wihtspace

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -9,21 +9,16 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(str string) (result string, err error) {
-
 	var slash bool
-
 	for i, symbol := range str {
-
 		if symbol == '\\' && !slash {
 			slash = true
 			continue
 		}
-
 		if slash {
 			if unicode.IsLetter(symbol) {
 				return "", ErrInvalidString
 			}
-
 			result += string(symbol)
 			slash = false
 			continue
@@ -33,25 +28,20 @@ func Unpack(str string) (result string, err error) {
 			if i == 0 {
 				return "", ErrInvalidString
 			}
-
 			if unicode.IsDigit([]rune(str)[i-1]) && []rune(str)[i-2] != '\\' {
 				return "", ErrInvalidString
 			}
-
 			val, _ := strconv.Atoi(string(symbol))
 			if val == 0 {
 				result = result[:len(result)-1]
 				continue
 			}
-
 			for j := 0; j < val-1; j++ {
 				result += string([]rune(str)[i-1])
 			}
 			continue
 		}
-
 		result += string(symbol)
 	}
-
 	return result, nil
 }
