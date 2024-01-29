d4f213a088a14d92198173cc3bbbe5eda96c9f0b
nikita <nikitanuc@yandex.ru>
2024-01-30 01:31:59 +0300

    *fix

--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -132,18 +132,6 @@ func TestValidate(t *testing.T) {
 			expectedErrAs: nil,
 		},
 		{
-			name: "substructs",
-			in: App{
-				Version: "0.0.1",
-				User: User{
-					Age:   15,
-					ID:    "LEMZzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz",
-					Role:  "admin",
-					Email: "test2@gmail.com",
-				},
-			},
-		},
-		{
 			name: "substructs negative",
 			in: App{
 				Version: "0.0.1",
