dab60bbdfe8ed4acb0b6ac2826397fa77c6b75d5
nikita <nikitanuc@yandex.ru>
2024-01-31 20:47:22 +0300

    *fix

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,47 +1,48 @@
-//go:build !bench
-// +build !bench
-
 package hw10programoptimization
 
 import (
-	"bytes"
-	"testing"
+	"bufio"
+	"fmt"
+	"io"
+	"regexp"
+	"strings"
 
-	"github.com/stretchr/testify/require"
+	"github.com/valyala/fastjson"
 )
 
-var data = `{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"aliquid_qui_ea@Browsedrive.gov","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}
-{"Id":2,"Name":"Jesse Vasquez","Username":"qRichardson","Email":"mLynch@broWsecat.com","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}
-{"Id":3,"Name":"Clarence Olson","Username":"RachelAdams","Email":"RoseSmith@Browsecat.com","Phone":"988-48-97","Password":"71kuz3gA5w","Address":"Monterey Park 39"}
-{"Id":4,"Name":"Gregory Reid","Username":"tButler","Email":"5Moore@Teklist.net","Phone":"520-04-16","Password":"r639qLNu","Address":"Sunfield Park 20"}
-{"Id":5,"Name":"Janice Rose","Username":"KeithHart","Email":"nulla@Linktype.com","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"}`
-
-func TestGetDomainStat(t *testing.T) {
-
-	t.Run("find 'com'", func(t *testing.T) {
-		result, err := GetDomainStat(bytes.NewBufferString(data), "com")
-		require.NoError(t, err)
-		require.Equal(t, DomainStat{
-			"browsecat.com": 2,
-			"linktype.com":  1,
-		}, result)
-	})
-
-	t.Run("find 'gov'", func(t *testing.T) {
-		result, err := GetDomainStat(bytes.NewBufferString(data), "gov")
-		require.NoError(t, err)
-		require.Equal(t, DomainStat{"browsedrive.gov": 1}, result)
-	})
-
-	t.Run("find 'unknown'", func(t *testing.T) {
-		result, err := GetDomainStat(bytes.NewBufferString(data), "unknown")
-		require.NoError(t, err)
-		require.Equal(t, DomainStat{}, result)
-	})
+type User struct {
+	ID       int
+	Name     string
+	Username string
+	Email    string
+	Phone    string
+	Password string
+	Address  string
 }
 
-func BenchmarkGetDomainStat(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		_, _ = GetDomainStat(bytes.NewBufferString(data), "com")
+type (
+	DomainStat map[string]int
+)
+
+var regexpForDomain = regexp.MustCompile(`[^@]+$`)
+
+func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
+	regexpForEmail := regexp.MustCompile(`\.` + domain)
+	result := make(DomainStat)
+	scan := bufio.NewScanner(r)
+
+	for i := 0; scan.Scan(); i++ {
+		email := fastjson.GetString(scan.Bytes(), "Email")
+
+		if regexpForEmail.MatchString(email) {
+			domain := strings.ToLower(regexpForDomain.FindString(email))
+			result[domain]++
+		}
 	}
+
+	if err := scan.Err(); err != nil {
+		return nil, fmt.Errorf("scanner error: %w", err)
+	}
+
+	return result, nil
 }
